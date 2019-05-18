79d1ebe346b930d753aba0074d20410decfa152d
Axel S <saxelsyeguillaume@gmail.com>
2019-05-18 11:24:01 +0200

    RoomieCreate vue2

---
+++ src/ITI.Roomies.Spa/src/components/Roomies/RoomiesCreate2.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="container">
+<header>
+  <H2></H2>
+</header>
+
+<main>
+  <form>
+    <div class="alert alert-danger" v-if="errors.length > 0">
+
+    </div>
+    <button class="btn btn-dark" @click="onSubmit">Sauvegarder</button>
+  </form>
+</main>
+
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  async mounted() {
+
+  },
+
+  methods: {
+
+  }
+  
+}
+</script>
+<style  lang="scss"scoped>
+.container{
+  margin-left: 0;
+  // margin-left: 0;
+  margin-right: 0;
+}
+</style>
